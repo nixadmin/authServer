e19d8be9db93a3aa0cea39a6beb6f192fef8793f
Evhenii Yankovii <yevgeniy.yankovoy@nixsolutions.com>
2017-01-18 15:40:47 +0200

    removed port

--- IdentityServer/src/AuthServerDemo/Program.cs
+++ IdentityServer/src/AuthServerDemo/Program.cs
@@ -12,7 +12,6 @@ namespace AuthServerDemo
 
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseUrls("http://localhost:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
